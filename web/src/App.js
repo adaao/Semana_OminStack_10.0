339f7c8bbfbb1c58d26eb55e11fef08b6fb2d243
adaao <armascarelli@gmail.com>
2020-01-17 08:27:11 -0300

    entendendo Componente

---
+++ web/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//Entendendo React
+
+//Componente: funão que retorna algum conteúdo
+
+//Estado
+
+//Propriedade
+
+function App() {
+  return (
+    <h1>Hello, World!</h1>
+  );
+}
+
+export default App;
