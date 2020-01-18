78720bdc88b6b8d9f8c60bcfc1cf2855f234a5dc
adaao <armascarelli@gmail.com>
2020-01-17 23:08:04 -0300

    sidebar

--- web/src/App.js
+++ web/src/App.js
@@ -1,17 +1,42 @@
-import React, { useState } from 'react';
+import React from 'react';
+import './global.css';
+import './App.css';
+import './Sidebar.css';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  
-  function incrementCounter(){
-    setCounter(counter + 1);
-  }
-  
   return (
-    <>
-      <h1>Counter {counter}</h1>
-      <button onClick={incrementCounter}>Incrementar</button>
-    </>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="username_github">Usuário do Github</label>
+            <input name="github_username" id="username_github"required/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs"required/>
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude"required/>
+            </div>
+            
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude"required/>
+            </div>
+          </div>
+          <button type="submit" className="botao_de_salvar">Salvar</button>
+        </form>
+      </aside>
+      <main>
+
+      </main>
+    </div>
   );
 }
 
